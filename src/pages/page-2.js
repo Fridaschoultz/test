e4f51d1a0ee3bdde5ef271927204db258c3c2633
Frida Schoultz <fridaschoultz@hotmail.com>
2019-08-28 13:58:20 +0200

    commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,6 +9,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hello there!</h1>
     <p>email us at: yomama@hotmail.com</p>
+    <p>Thank you!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
